fefc14cf3c4b62ec6986410c750ccea621f1885f
KarunaShrestha02 <karuna100502@gmail.com>
2024-11-18 14:54:06 +0545

    connected server client

--- weather-station/src/lib/supabase/server.ts
+++ weather-station/src/lib/supabase/server.ts
@@ -1,37 +1,29 @@
-"use server";
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
 
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { cookies } from "next/headers";
-
-export default async function createSupabaseServerClient() {
-  const cookieStore = cookies();
-
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-  if (!supabaseUrl || !supabaseAnonKey) {
-    throw new Error("Missing Supabase URL or Anon Key environment variables");
-  }
+export async function createClient() {
+  const cookieStore =  cookies()
 
   return createServerClient(
-    supabaseUrl,
-    supabaseAnonKey,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
-          // Returns all cookies as an array of { name: string, value: string }
-          return cookieStore.getAll().map(cookie => ({
-            name: cookie.name,
-            value: cookie.value,
-          }));
+          return cookieStore.getAll()
         },
-        setAll(cookiesArray: { name: string, value: string, options: CookieOptions }[]) {
-          // Sets all cookies from the array provided
-          cookiesArray.forEach(({ name, value, options }) => {
-            cookieStore.set({ name, value, ...options });
-          });
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
         },
       },
     }
-  );
+  )
 }
