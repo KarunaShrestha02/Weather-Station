6efa393d5cb05bdf44172d26669fd9e9499e254a
KarunaShrestha02 <karuna100502@gmail.com>
2024-11-29 15:23:51 +0545

    updated main page

--- weather-station/src/app/page.tsx
+++ weather-station/src/app/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
-//import { Footer, Header, Navbar } from "../components";
-//import SplineComponent from "../components/SplineComponent";
+import { Navbar } from "@/components";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+import SplineComponent from "../components/SplineComponent";
 import readUserSession from "@/actions";
 
 export default async function Home() {
@@ -12,12 +14,12 @@ export default async function Home() {
     <div className="bg-gradient-to-t from-[#111627] to-[#344378] h-[900px] md:h-auto">
       <div className="absolute w-full h-full text-white flex flex-col justify-between">
         <div className="flex flex-col">
-          {/*<Navbar />
+          <Navbar />
           <Header />
         </div>
-        <Footer />*/}
+        <Footer />
       </div>
-      {/*<SplineComponent/>*/}</div>
+      <SplineComponent/>
     </div>
   );
 }
